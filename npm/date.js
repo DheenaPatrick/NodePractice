93d16343598fa39177ea4b3b9838cdccf6024db2
= <=>
2025-03-13 11:14:47 +0530

    Practiced npm and worked with 2 packages

---
+++ npm/date.js
@@ -0,0 +1,10 @@
+const {format} = require('date-fns')
+
+const {v4:uuid} = require('uuid')//v4 of uuid.
+
+
+console.log(format(new Date(2014, 1, 11), "yyyy-MM-dd"));
+
+
+console.log(uuid);
+console.log("hell0");
